37fa49e562b8123c9c4f1d29f8019c961fe1f1da
stanjae <stanykhay29@gmail.com>
2022-10-13 19:58:34 -0700

    New files on C language

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %i bytes\n", sizeof(char));
+	printf("Size of an integer: %i bytes\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
